c47a6d86826e9ae09527cc00f8a3d35eaba6f0ea
rickastlee <45075899+rickastlee@users.noreply.github.com>
2024-04-18 10:52:07 +0200

    Bitwise: Improve indentation
    Use tabs instead of 2 spaces

--- Bitwise.java
+++ Bitwise.java
@@ -1,18 +1,18 @@
 public class Main {
-  public static void main(String[] args) {
-    int maxLen = 4;
-    int[] tests = { 0, 1, 2, 3, 4 };
+	public static void main(String[] args) {
+		int maxLen = 4;
+		int[] tests = { 0, 1, 2, 3, 4 };
 
-    for (int i = 0; i < maxLen - 1; i++) {
-      test(i, tests);
-    }
-  }
+		for (int i = 0; i < maxLen - 1; i++) {
+			test(i, tests);
+		}
+}
 
-  private static void test(int mask, int[] testArray) {
-    for (int i = 0; i < testArray.length; i++) {
-      int val = testArray[i];
-      System.out.println(String.format("%d & %d = %b",
-          mask, val, (mask & val) == 1));
-    }
-  }
+	private static void test(int mask, int[] testArray) {
+		for (int i = 0; i < testArray.length; i++) {
+			int val = testArray[i];
+			System.out.println(String.format("%d & %d = %b",
+					mask, val, (mask & val) == 1));
+		}
+	}
 }
